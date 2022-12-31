ecdd5653d6a95d8f8ced700ddaa8abb5bf2c67a9
Daniel Montilla <montilla.d2000@gmail.com>
2022-12-31 17:48:43 -0400

    gh & deploy setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/interval-timer/",
   plugins: [
     vue(),
     VitePWA({
@@ -22,12 +23,12 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: '/app/android-chrome-192x192.png',
+            src: '/interval-timer/app/android-chrome-192x192.png',
             sizes: '192x192',
             type: 'image/png',
           },
           {
-            src: '/app/android-chrome-512x512.png',
+            src: '/interval-timer/app/android-chrome-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
